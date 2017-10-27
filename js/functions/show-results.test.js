db08d220b86743e3fd56cdb1b64da4aa0b551e07
lalalakolpachok <akolpakova91@gmail.com>
2017-10-27 04:08:49 +0300

    travis#2

--- js/functions/show-results.test.js
+++ js/functions/show-results.test.js
@@ -1,50 +1,51 @@
 import assert from 'assert';
-import {resultScreenContent, showResults} from './show-results';
+import showResults from './show-results';
+import {resultScreenContent} from '../data/results';
 
 describe(`Функция вывода результата`, () => {
   it(`должна правильно вычислить результаты игрока в случае выигрыша`, () => {
-    let allScores = [4, 7, 5, 20];
     let playersResult = {
       score: 12,
       attemptsLeft: 2,
-      TIME: 30000
+      time: 30
     };
-    assert.equal(showResults(allScores, playersResult), resultScreenContent.win(2, 5, 60));
+    let allScores = [4, 7, 5, 20, playersResult.score];
+    assert.equal(showResults(allScores, playersResult), resultScreenContent.win.message2(2, 5, 60));
 
-    allScores = [5, 12, 12, 6, 8, 9, 4, 13, 15, 4, 7, 5, 12, 20];
     playersResult = {
       score: 20,
       attemptsLeft: 4,
-      TIME: 300
+      time: 300
     };
-    assert.equal(showResults(allScores, playersResult), resultScreenContent.win(1, 15, 93));
+    allScores = [5, 12, 12, 6, 8, 9, 4, 13, 15, 4, 7, 5, 12, 20, playersResult.score];
+    assert.equal(showResults(allScores, playersResult), resultScreenContent.win.message2(1, 15, 93));
 
-    allScores = [5, 6, 8, 5, 10];
     playersResult = {
       score: 4,
       attemptsLeft: 1,
-      TIME: 3300
+      time: 3300
     };
-    assert.equal(showResults(allScores, playersResult), resultScreenContent.win(6, 6, 0));
+    allScores = [5, 6, 8, 5, 10, playersResult.score];
+    assert.equal(showResults(allScores, playersResult), resultScreenContent.win.message2(6, 6, 0));
   });
 
-  it(`должна вернуть '${resultScreenContent.timeOut}', если время вышло`, () => {
+  it(`должна вернуть '${resultScreenContent.timeOut.message}', если время вышло`, () => {
     let allScores = [4, 7, 5, 12, 20];
     let playersResult = {
       score: 2,
       attemptsLeft: 2,
-      TIME: 0
+      time: 0
     };
-    assert.equal(showResults(allScores, playersResult), resultScreenContent.timeOut);
+    assert.equal(showResults(allScores, playersResult), resultScreenContent.timeOut.message);
   });
 
-  it(`должна вернуть '${resultScreenContent.attemptsOut}', если закончились попытки`, () => {
+  it(`должна вернуть '${resultScreenContent.attemptsOut.message}', если закончились попытки`, () => {
     let allScores = [4, 7, 5, 12, 20];
     let playersResult = {
       score: 2,
       attemptsLeft: 0,
-      TIME: 50000
+      time: 50
     };
-    assert.equal(showResults(allScores, playersResult), resultScreenContent.attemptsOut);
+    assert.equal(showResults(allScores, playersResult), resultScreenContent.attemptsOut.message);
   });
 });
