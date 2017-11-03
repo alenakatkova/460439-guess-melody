5a140da4f331a67994cf61e7b856c555cb8e183c
lalalakolpachok <akolpakova91@gmail.com>
2017-11-03 15:01:13 +0300

    app - location.hash

--- js/application.js
+++ js/application.js
@@ -1,17 +1,71 @@
 import startScreen from './screens/start/start-screen';
 import questionScreen from './screens/question/question-screen';
 import resultScreen from './screens/result/result-screen';
+import {initialState} from './data/game-data';
+
+const ControllerId = {
+  START: ``,
+  QUESTION: `question`,
+  RESULT: `result`
+};
+
+const getDataForHash = (state) => {
+  return {
+    level: state.currentQuestionIndex + 1,
+    mistakes: state.mistakes,
+    time: state.time};
+};
+
+const saveState = (state) => {
+  return JSON.stringify(getDataForHash(state));
+};
+
+const loadState = (dataString) => {
+  try {
+    return JSON.parse(dataString);
+  } catch (e) {
+    return initialState;
+  }
+};
+
+const routes = {
+  [ControllerId.START]: startScreen,
+  [ControllerId.QUESTION]: questionScreen,
+  [ControllerId.RESULT]: resultScreen
+};
 
 export default class App {
+  static init() {
+    const hashChangeHandler = () => {
+      const hashValue = location.hash.replace(`#`, ``);
+      const [id, data] = hashValue.split(`?`);
+      this.changeHash(id, data);
+    };
+    window.onHashChange = hashChangeHandler;
+    hashChangeHandler();
+  }
+
+  static changeHash(id, data) {
+    const controller = routes[id];
+    if (controller) {
+      controller.init(loadState(data));
+    }
+  }
+
   static showWelcome() {
     startScreen.init();
+    location.hash = ControllerId.START;
   }
 
   static showQuestion(state) {
     questionScreen.init(state);
+    location.hash = `${ControllerId.QUESTION}?${saveState(state)}`;
   }
 
-  static showResult(resultType, mainMessage, extraMessage) {
-    resultScreen.init(resultType, mainMessage, extraMessage);
+  static showResult(resultType, state, gameResults) {
+    resultScreen.init(resultType, state, gameResults);
+    location.hash = `${ControllerId.RESULT}?${saveState(state)}`;
   }
 }
+
+App.init();
